8145791b6a2947618cc1e8e425077105ba61c6db
davidparys <davidparys@users.noreply.github.com>
2022-04-24 18:48:51 +0000

    deploy: 509516f93bea55ebcbb9c13e971bf518309b8289

---
+++ _next/static/NdpJRlKpKccgD0scqXhnN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/98-47d333fbea5e53e5.js","static/chunks/pages/index-fffc321c56143570.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
